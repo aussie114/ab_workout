01119752808b7b666a1df4f817fe20ec1820141f
aussie114 <redacted@example.com>
2025-10-21 17:47:38 +1000

    added fullscreen stretching

--- main/process.c
+++ main/process.c
@@ -4,112 +4,34 @@
 #include "buttons.h"
 #include "exercises.h"
 
+extern RenderTexture2D virtual_screen;
+
 void process(void)
 {
 	if (IsKeyPressed(KEY_S))
 	{
 		PlaySound(beep);
 	}
+	// Render to virtual screen
+	BeginTextureMode(virtual_screen);
+		ClearBackground(RAYWHITE);
+		process_keyboard();
+		process_exercises();
+		process_buttons();
+	EndTextureMode();
+
+	// Render to actual screen
 	BeginDrawing();
-	ClearBackground(RAYWHITE);
-	process_keyboard();
-	process_exercises();
-	process_buttons();
+		ClearBackground(RAYWHITE);
+		DrawTexturePro
+		(
+			virtual_screen.texture,
+			(Rectangle){0.0f, 0.0f, 1280.0f, -720.0f},                             // Source
+			(Rectangle){0, 0, (float)GetScreenWidth(), (float)GetScreenHeight()},  // Destination
+			(Vector2){ 0, 0 },                                                     // Origin
+			0.0f,                                                                  // Rotation
+			WHITE                                                                  // Tint
+		);
 	EndDrawing();
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-#include "raylib.h"
-#include "sound.h"
-#include "exercises.h"
-
-Vector2 mouse_position = {0,0};
-int button_border_size = 4;
-Color button_colour = {25, 116, 186, 255};
-Rectangle previous_button        = {1146, 14, 120, 120};
-Rectangle next_button     = {1142, 582, 128, 128};
-
-
-void process(void)
-{
-	if (audio_loaded == 0)
-	{
-	    BeginDrawing();
-		ClearBackground(RAYWHITE);
-		DrawText("press a to continue...", 0, 0, 100, BLACK);
-
-		if (GetKeyPressed() == 'A')
-		{
-			InitAudioDevice();
-//			load_sounds();
-			audio_loaded = 1;
-		}
-	    EndDrawing();
-		return;	
-	}
-
-    BeginDrawing();
-
-	mouse_position = GetMousePosition();
-	ClearBackground(RAYWHITE);
-
-	process_exercises();
-
-	// Previous button
-	if (CheckCollisionPointRec(mouse_position, previous_button))
-	{
-		DrawRectangleRec(previous_button, button_colour);
-		if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && stage > 0)
-		{
-			exercises[stage].time_remaining = exercises[stage].interval + 3;
-			stage--;
-		}
-	}
-	else
-	{
-		DrawRectangleRec(previous_button, BLACK);
-		DrawRectangle(
-			previous_button.x + button_border_size, 
-			previous_button.y + button_border_size,
-			previous_button.width - button_border_size * 2,
-			previous_button.height - button_border_size * 2,
-			button_colour
-		);
-	}
-
-
-
-	// next_button
-	if (CheckCollisionPointRec(mouse_position, next_button))
-	{
-		DrawRectangleRec(next_button, GREEN);
-		if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && stage < STAGES - 1)
-		{
-			exercises[stage].time_remaining = exercises[stage].interval + 3;
-			stage++;
-		}
-	}
-	else
-	{
-		DrawRectangleRec(next_button, BLACK);
-	}
-
-    EndDrawing();
-}
-*/
--- main/setup.c
+++ main/setup.c
@@ -3,9 +3,12 @@
 #include "sounds.h"
 #include "exercises.h"
 
+RenderTexture2D virtual_screen;
+
 void setup(bool web)
 {
     InitWindow(1280, 720, "game");
+	virtual_screen = LoadRenderTexture(1280, 720);
 	if (!web) {SetTargetFPS(60);}
 	InitAudioDevice();
 	load_sounds();
